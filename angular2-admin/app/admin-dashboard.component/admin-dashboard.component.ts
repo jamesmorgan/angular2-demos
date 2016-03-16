5f67313c5d8b9df4977097c353bc9e0eedea3aab
James Morgan <james.morgan.e@googlemail.com>
2016-03-16 22:51:20 +0000

    Adding both examples of an observable and and event emitter

--- angular2-admin/app/admin-dashboard.component/admin-dashboard.component.ts
+++ angular2-admin/app/admin-dashboard.component/admin-dashboard.component.ts
@@ -4,6 +4,8 @@ import {Competition} from "../core/domain/Competition";
 import {EventEmitter} from "angular2/core";
 import {CompetitionsService} from "../core/services/CompetitionsService";
 import {OnDestroy} from "angular2/core";
+import {OnInit} from "angular2/core";
+import {Subscription} from "rxjs/Subscription";
 
 @Component({
     selector: 'admin-dashboard',
@@ -13,22 +15,39 @@ import {OnDestroy} from "angular2/core";
         CompetitionsListComponent
     ]
 })
-export class AdminDashboardComponent implements OnDestroy {
+export class AdminDashboardComponent implements OnDestroy, OnInit {
 
     /** Public data */
     competitions:Competition[];
 
+    /** Public data */
+    competitionsObservable:Competition[];
+
     /** Subscriber */
-    private _subscription:EventEmitter<Competition[]>;
+    private _competitionsEventHandler:EventEmitter<Competition[]>;
+    private _competitionsSubscription:Subscription;
 
     constructor(private _competitionsService:CompetitionsService) {
-        this._subscription =
+
+        // Get a handle on the event emitter to react on the changes
+        this._competitionsEventHandler =
             this._competitionsService.onCompetitionsChanged.subscribe((competitions) => {
                 this.competitions = competitions;
             });
+
+        // Subscribe an changes which may happen
+        this._competitionsSubscription = this._competitionsService.competitionsChanged$.subscribe((competitions) => {
+            this.competitionsObservable = competitions;
+        })
+    }
+
+    ngOnInit():any {
+        return undefined;
     }
 
     ngOnDestroy():any {
-        return this._subscription.unsubscribe();
+        // prevent memory leak when component destroyed
+        this._competitionsSubscription.unsubscribe();
+        return this._competitionsEventHandler.unsubscribe();
     }
 }
--- angular2-admin/app/core/services/CompetitionsService.ts
+++ angular2-admin/app/core/services/CompetitionsService.ts
@@ -1,25 +1,44 @@
-import {Injectable} from "angular2/core";
+import {Injectable, EventEmitter} from "angular2/core";
 import {Http} from "angular2/http";
+import {Observable} from 'rxjs/Observable';
+import {Observer} from "rxjs/Observer";
+import 'rxjs/add/operator/share';
+
 import {Competition} from "../domain/Competition";
-import {EventEmitter} from "angular2/core";
+import {Subject} from "rxjs/Subject";
 
 @Injectable()
 export class CompetitionsService {
 
-    competitions:Competition[];
+    /** Internal model state */
+    private competitions:Competition[];
 
+    /** Public Publisher */
     onCompetitionsChanged = new EventEmitter<Competition[]>();
 
+    /** Private Observable sources **/
+    private _competitionsSource:Subject = new Subject<Competition[]>();
+
+    /** Public observer  **/
+    competitionsChanged$ = this._competitionsSource.asObservable().share(); // share() = This will allow multiple Subscribers to one Observable
+
     constructor(http:Http) {
+
+        // Get the data on creation
         http.get('http://localhost:8080/competitions')
-            .subscribe(res => {
-                if (res.ok) {
-                    console.log('Unable to subscribe to competitions', res.status, res.text());
-                } else {
-                    this.parseCompetitions(res.json());
+            .subscribe(
+                data => {
+                    this.parseCompetitions(data.json());
                     this.triggerCompetitionsChanged();
-                }
-            });
+                    this.publishAtObservables();
+                },
+                err => console.error('Failed to load competitions', err),
+                () => console.log('Loaded competitions')
+            );
+    }
+
+    private publishAtObservables():void {
+        this._competitionsSource.next(this.competitions); // Push a new copy to all Subscribers.
     }
 
     private triggerCompetitionsChanged():void {
