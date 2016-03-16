13001e7fbb872d85f68816e009d02fa6d9f9b1fa
James Morgan <james.morgan.e@googlemail.com>
2016-03-16 21:57:18 +0000

    Missing score property

--- angular2-admin/app/core/domain/Selection.ts
+++ angular2-admin/app/core/domain/Selection.ts
@@ -11,6 +11,7 @@ export class Selection {
 
     fromJson(selection) {
         this.selection = new ID(selection.selection);
+        this.score = selection.score;
         this.potIndex = selection.potIndex;
         this.multiplier = selection.multiplier;
         this.handicap = selection.handicap;
