a21ec10b2d8a33c1bf8242773b81b76b79f749e6
Andy Gray <andyg@cakesolutions.net>
2016-03-21 16:15:25 +0000

    align id and name

--- angular2-admin/app/core/domain/Selection.ts
+++ angular2-admin/app/core/domain/Selection.ts
@@ -2,20 +2,24 @@ import {ID} from "./ID";
 
 export class Selection {
 
-    selection:ID;
+    _id:ID;
+    name:String;
     potIndex:number;
     multiplier:number;
     handicap:number;
     score:number;
-    selectionName:String;
+    sport:String;
+    avatar:String;
 
     fromJson(selection) {
-        this.selection = new ID(selection.selection);
+        this._id = new ID(selection._id);
+        this.name = selection.name;
         this.score = selection.score;
         this.potIndex = selection.potIndex;
         this.multiplier = selection.multiplier;
         this.handicap = selection.handicap;
-        this.selectionName = selection.selectionName;
+        this.sport = selection.sport;
+        this.avatar = selection.avatar;
         return this;
     }
 }
